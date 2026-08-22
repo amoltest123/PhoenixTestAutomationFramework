c9aefc6f587e1d5e0d268cebf2dd4d1e5fc1f689
Amol Latthe <amollattheict@gmail.com>
2026-08-22 18:28:22 +0530

    Created Config Manager class for managing multiple environments (dev, qa, uat) for the project
    Updated pom.xml- Updated maven compiler source and target to 14.

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -1,24 +1,25 @@
 package com.api.tests;
 
-import static io.restassured.RestAssured.*;
-
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
 import org.testng.annotations.Test;
 
 import com.api.pojos.UserCredentials;
+import static com.api.utils.ConfigManager.*;
 
 import io.restassured.http.ContentType;
-import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class LoginAPITest {
 
 	@Test
-	public void loginAPITest() {
+	public void loginAPITest(){
 		
 		UserCredentials userCredentials = new UserCredentials("iamfd", "password");
 		given()
-			.baseUri("http://64.227.160.186:9000/v1")
+			.baseUri(getProperty("BASE_URI"))
 			.and()
 			.contentType(ContentType.JSON)
 			.and()
@@ -34,7 +35,7 @@ public class LoginAPITest {
 		.then()
 			.log().all()
 			.statusCode(200)
-			.time(lessThan(1000L))
+			.time(lessThan(2000L))
 			.and()
 			.body("message", equalTo("Success"))
 			.and()
---
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -0,0 +1,50 @@
+package com.api.utils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+
+	private static Properties prop = new Properties();
+	private static String path = "config/config.properties";
+	private static String env;
+
+	private ConfigManager() {
+
+	}
+
+	static {
+
+		env = System.getProperty("env", "qa");
+		env = env.toLowerCase().trim();
+		switch (env) {
+		case "dev" -> path = "config/config.dev.properties";
+		case "qa" -> path = "config/config.qa.properties";
+		case "uat" -> path = "config/config.uat.properties";
+		default -> path = "config/config.qa.properties";
+		}
+
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+
+		if (inputStream == null) {
+			throw new RuntimeException("Can not find the file at path " + path);
+		}
+		try {
+			prop.load(inputStream);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static String getProperty(String key) {
+		return prop.getProperty(key);
+	}
+}
